5bb2ef425de09ae52dd72d5321ad1b384e9a04bd
Dmitry <dmitry.izjurov@yandex.ru>
2022-04-28 20:19:05 +0300

    Выполнено задание Map

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,7 +1,13 @@
-export default function sum(items) {
-  let result = 0;
-  for (const item of items) {
-    result += item;
+export default class ErrorRepository {
+  constructor() {
+    this.dataError = new Map([[1, 'Файл не найден'], [2, 'Сетевая ошибка'], [3, 'Файл поврежден']]);
   }
-  return result;
+
+  translate(code) {
+    if (this.dataError.has(code) && typeof code === 'number') {
+      const result = this.dataError.get(code);
+      return result;
+    }
+    return 'Unknown error';
+  }
 }
